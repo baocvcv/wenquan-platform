300d7b35132ae32173a705d2b92543630502e0d1
baocvcv <baocvcv@gmail.com>
2019-09-29 21:46:51 +0800

    Made UserBase abstract

--- backend/models/user_base.py
+++ backend/models/user_base.py
@@ -1,5 +1,22 @@
+""" Base model for User """
 from django.db import models
 
 class UserBase(models.Model):
-    user_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    """ Base class of Users
+
+    Attributes:
+        @param user_name
+        @param email
+        @param password
+        @param user_type
+    """
+    user_id = models.AutoField()
+    username = models.CharField(max_length=50, primary_key=True)
+    email = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        "Stringify"
+        return self.username + "(%s)" % self.email
