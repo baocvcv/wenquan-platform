fbe77cf601ddf766ecd2e63341cc166fd2130e8a
夏奇 <hsiachi@MethaneB.local>
2019-09-29 15:56:54 +0800

    Add Plugin Vuetify for UI Framework #9

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div id="app">
+  <v-app id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/about">About</router-link> | 
       <router-link to="/admin">Admin</router-link>
     </div>
     <router-view />
-  </div>
+  </v-app>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
 
 <style>
 #app {
