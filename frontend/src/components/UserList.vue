8a444098ee5b45d71e6a8eb7f93c3035d63036fe
夏奇 <hsiachi@MethaneB.local>
2019-09-29 15:21:58 +0800

    Add the skeleton for admin index view #9

---
+++ frontend/src/components/UserList.vue
@@ -0,0 +1,8 @@
+<template>
+</template>
+
+<script>
+export default {
+    name: "user-list",
+}
+</script>
---
+++ frontend/src/views/AdminIndex.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <user-list>
+        </user-list>
+    </div>
+</template>
+
+<script>
+import user_list from "@/components/UserList.vue";
+
+export default {
+    name: "admin-index",
+    components: {
+        user_list
+    }
+}
+</script>
