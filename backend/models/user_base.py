a540b3f849ffd03d60b9260ff809437b19b67f6e
baocvcv <baocvcv@gmail.com>
2019-09-29 22:48:45 +0800

    Added Admin, SuperAdmin and Students

--- backend/models/user_base.py
+++ backend/models/user_base.py
@@ -5,14 +5,18 @@ class UserBase(models.Model):
     """ Base class of Users
 
     Attributes:
-        @param user_name
+        @param user_id
+        @param username
         @param email
         @param password
         @param user_type
     """
     user_id = models.AutoField()
     username = models.CharField(max_length=50, primary_key=True)
-    email = models.CharField(max_length=100, unique=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=256)
+    user_icon = models.CharField(max_length=254)
+    register_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
@@ -20,3 +24,27 @@ class UserBase(models.Model):
     def __str__(self):
         "Stringify"
         return self.username + "(%s)" % self.email
+
+class Admin(UserBase):
+    """ General administrators of the site
+
+    Attributes:
+    """
+
+    pass
+
+
+class SuperAdmin(Admin):
+    """ Super admin
+
+    """
+
+    pass
+
+class Students(UserBase):
+    """ Student users
+    """
+    
+    school_name = models.CharField(max_length="100")
+    #TODO: define Tiku and possible intermediaries
+    #authorizations = models.ManyToManyField(Tiku)
