55b6e9cf35e9a9c40312555014b8608c79ab8b66
鄢涤非 <ydf18@mails.tsinghua.edu.cn>
2019-09-30 09:31:12 +0800

    Create the base view and component and link them. #12

---
+++ frontend/src/components/SignInBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="sign-in">
+    <input type="input" v-model="username" /><br />
+    <input type="password" v-model="password" /><br />
+    <input type="submit" v-on:click="click" value="Sign In" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sign-in",
+  data: function() {
+    return {
+      username: "",
+      password: "",
+      userType: "Normal"
+    };
+  },
+  methods: {
+    click: function() {
+      alert("fa Q");
+    }
+  }
+};
+</script>
---
+++ frontend/src/views/SignIn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Sign In</h2>
+    <sign-in-box></sign-in-box>
+  </div>
+</template>
+
+<script>
+import SignInBox from "../components/SignInBox.vue";
+
+export default {
+  name: "sign-in",
+  components: {
+    "sign-in-box": SignInBox
+  }
+};
+</script>
