019910bbc966cc0301f0308b43a3904097965e65
孔祥哲 <2018011997@secoder.net>
2019-09-30 10:41:32 +0800

    Update component SignUpBox, add data dictionary #13

--- frontend/src/components/SignUpBox.vue
+++ frontend/src/components/SignUpBox.vue
@@ -2,11 +2,11 @@
   <div id="sign-up-box">
     <p>
       <label>用户名: </label>
-      <input type="text" />
+      <input type="text" v-model="user_name" />
     </p>
     <p>
       <label>密码: </label>
-      <input type="password" />
+      <input type="password" v-model="password" />
     </p>
     <p>
       <label>确认密码: </label>
@@ -14,11 +14,14 @@
     </p>
     <p>
       <label>邮箱: </label>
-      <input type="text" />
+      <input type="text" v-model="email" />
     </p>
     <p>
-      <input type="checkbox" />
-	  <label>我接受并同意《用户服务条款》和《隐私相关政策》</label>
+      <input type="checkbox" v-model="accept_terms" />
+      <label>我接受并同意《用户服务条款》和《隐私相关政策》</label>
+    </p>
+    <p>
+      <input type="submit" v-on:click="click" />
     </p>
   </div>
 </template>
@@ -26,15 +29,25 @@
 <script>
 export default {
   name: "sign-up-box",
-  props: {
-    a: Array
+  data: function() {
+    return {
+      user_name: "",
+      password: "",
+      email: "",
+      accepte_terms: false
+    };
   },
-  components: {
-  },
-}
+  methods: {
+    click: function() {
+      alert("shit");
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#sign-up-box {
+  border: 5px dashed grey;
+}
 </style>
-
