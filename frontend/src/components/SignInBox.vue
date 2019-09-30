98f5b33936b89b3ed8daf3866bee0c30c3ab9d38
鄢涤非 <2018010258@secoder.net>
2019-09-30 22:17:04 +0800

    Modify sign in format and add MD5 for password encryption. #12

--- frontend/src/components/SignInBox.vue
+++ frontend/src/components/SignInBox.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="sign-in">
-    <input type="input" v-model="username" /><br />
-    <input type="password" v-model="password" /><br />
-    <input type="submit" v-on:click="click" value="Sign In" />
+    <table>
+      <tr>
+        <td>用户名：</td>
+        <td><input type="input" v-model="username" /></td>
+      </tr>
+      <tr>
+        <td>密码：</td>
+        <td><input type="password" v-model="password" /></td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <input type="submit" v-on:click="click" value="Sign In" />
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import md5 from "js-md5";
+
 export default {
   name: "sign-in",
   data: function() {
@@ -18,8 +32,14 @@ export default {
   },
   methods: {
     click: function() {
-      alert("fa Q");
+      alert("usr:" + this.username + "\npsw(md5):" + md5(this.password));
     }
   }
 };
 </script>
+
+<style>
+.sign-in table {
+  margin: auto;
+}
+</style>
