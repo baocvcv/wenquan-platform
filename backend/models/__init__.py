c2d12eb3a61cadd8f28e69e79ec1acb5e2495de4
Chen_Yuxiang <chenyuxi18@mails.tsinghua.edu.cn>
2019-10-02 16:40:45 +0800

    Modified questions related model for polymorphic --Cyx #16

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,6 +1,5 @@
 """import all models here to make models package"""
 
-from .question import Question
-from .choice_question import ChoiceQ
-from .choice_question import SingleChoiceQ
-from .choice_question import MultpChoiceQ
+from .questions.choice_question import ChoiceQ
+from .questions.choice_question import SingleChoiceQ
+from .questions.choice_question import MultpChoiceQ
