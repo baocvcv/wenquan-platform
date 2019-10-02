bb4a5ec7654802e653bc51f530757b35dab13b78
鄢涤非 <2018010258@secoder.net>
2019-10-02 15:15:45 +0800

    Add support for material design icons as cdn cannot be used. #12

--- frontend/src/components/SignInBox.vue
+++ frontend/src/components/SignInBox.vue
@@ -10,7 +10,8 @@
           label="密码"
           v-bind:type="show_password ? 'text' : 'password'"
           v-bind:rules="[v => !!v || 'Password cannot be empty']"
-          v-bind:append-icon="show_password ? 'visibility' : 'visibility_off'"></v-text-field>
+          v-bind:append-icon="show_password ? 'visibility' : 'visibility_off'"
+          v-on:click:append="show_password=!show_password"></v-text-field>
         <v-btn v-on:click="click" v-bind:disabled="!username || !password">Sign In</v-btn>
     </v-form>
   </div>
