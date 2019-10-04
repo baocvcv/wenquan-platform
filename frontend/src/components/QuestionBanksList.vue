ed3e603f948c3222c9d1e141ea38941312f61780
孔祥哲 <2018011997@secoder.net>
2019-10-04 21:53:53 +0800

    Add tab structure for question bank list view #28

--- frontend/src/components/QuestionBanksList.vue
+++ frontend/src/components/QuestionBanksList.vue
@@ -1,48 +1,47 @@
 <template>
   <div>
-	<v-list two-line subheader>
-	  <v-subheader inset>Question Banks</v-subheader>
+    <v-list two-line>
+      <v-list-item
+        v-for="qst_bank in question_banks"
+        :key="qst_bank.name"
+        @click="redirct('/questionbanks/' + qst_bank.id)"
+      >
+        <v-list-item-avatar>
+          <v-icon v-text="qst_bank.icon"></v-icon>
+        </v-list-item-avatar>
 
-	  <v-list-item
-	    v-for="qst_bank in question_banks"
-		:key="qst_bank.name"
-		@click="redirct('/questionbanks/'+qst_bank.id)"
-	  >
-	    <v-list-item-avatar>
-		  <v-icon v-text="qst_bank.icon"
-		  ></v-icon>
-		</v-list-item-avatar>
+        <v-list-item-content align="left">
+          <v-list-item-title v-text="qst_bank.name"></v-list-item-title>
+          <v-list-item-subtitle v-text="qst_bank.brief"></v-list-item-subtitle>
+        </v-list-item-content>
 
-		<v-list-item-content align="left">
-		  <v-list-item-title v-text="qst_bank.name"></v-list-item-title>
-		  <v-list-item-subtitle v-text="qst_bank.brief"></v-list-item-subtitle>
-		</v-list-item-content>
+        <v-list-item-action @click.stop="">
+          <v-btn
+            icon
+            @click="
+              detail = true;
+              cur_qst_bank = qst_bank;
+            "
+          >
+            <v-icon color="grey lighten-1">mdi-information</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
 
-		<v-list-item-action @click.stop="">
-		  <v-btn icon @click="detail = true; test=1; cur_qst_bank = qst_bank;">
-			<v-icon color="grey lighten-1">mdi-information</v-icon>
-		  </v-btn>
-		
-		
-		</v-list-item-action>
-	  </v-list-item>
-	</v-list>
+    <!--detailed infomation for question banks-->
+    <v-dialog v-model="detail" max-width="500">
+      <v-list subheader>
+        <v-subheader inset>details of {{ cur_qst_bank.name }}</v-subheader>
 
-	<!--detailed infomation for question banks-->
-	  <v-dialog v-model="detail">
-	    <v-list subheader>
-	      <v-subheader inset>details of {{cur_qst_bank.name}}</v-subheader>
-
-	      <v-list-item v-for="(value, attr) in cur_qst_bank.details" :key="attr">
-	  	<v-list-item-content>{{attr}}: {{cur_qst_bank.details[attr]}}</v-list-item-content>
-	  	</v-list-item>
-	    </v-list>
-	    <v-btn
-	  	color="primary"
-	  	dark
-	  	@click="detail = false"
-	    >Back</v-btn>
-	  </v-dialog>
+        <v-list-item v-for="(value, attr) in cur_qst_bank.details" :key="attr">
+          <v-list-item-content>
+            {{ attr }}: {{ cur_qst_bank.details[attr] }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-btn color="primary" dark @click="detail = false">Back</v-btn>
+    </v-dialog>
   </div>
 </template>
 
@@ -50,23 +49,21 @@
 export default {
   name: "",
   props: {
-    question_banks: {} 
+    question_banks: {}
   },
-  data: function(){
-	return {
-	  detail: false,
-	  cur_qst_bank:{},
-	};
+  data: function() {
+    return {
+      detail: false,
+      cur_qst_bank: {}
+    };
   },
   methods: {
-	redirct(path) {
-	  this.$router.replace(path);
-	}
+    redirct(path) {
+      this.$router.replace(path);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
-
+<style scoped></style>
--- frontend/src/views/QuestionBanks.vue
+++ frontend/src/views/QuestionBanks.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
-	<h1>shit</h1>
-    <question-banks-list :question_banks="question_banks"></question-banks-list>
+    <v-card>
+      <v-toolbar flat color="primary" dark>
+        <v-toolbar-title>Question Banks</v-toolbar-title>
+      </v-toolbar>
+
+      <v-tabs vertical>
+        <v-tab align="left">
+          <v-icon>mdi-book-open</v-icon>
+          Browse
+        </v-tab>
+        <v-tab align="left">
+          <v-icon>mdi-folder-plus</v-icon>
+          Create
+        </v-tab>
+
+        <v-tab-item>
+          <question-banks-list
+            :question_banks="question_banks"
+          ></question-banks-list>
+        </v-tab-item>
+        <v-tab-item>
+          <h1>still in develop</h1>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
   </div>
 </template>
 
@@ -11,22 +34,21 @@ import QuestionBanksList from "../components/QuestionBanksList.vue";
 export default {
   name: "",
   data: function() {
-	return {
-	  question_banks: [
-	    {name: "kxz1", id: "1", icon: "mdi-folder", brief: "hahahahha", details: {questions: "100", difficulty: "5", content: "this is all bullshit"}},
-		{name: "kxz2", id: "2", icon: "mdi-folder", brief: "hohohoho", details: {questions: "1", difficulty: "4", content: "get out"}},
-		{name: "kxz3", id: "3", icon: "mdi-folder", brief: "hehehehe"},
-		{name: "kxz4", id: "4", icon: "mdi-folder", brief: "hihihihi"},
-		{name: "kxz5", id: "5", icon: "mdi-folder", brief: "huhuhuhu"}
-	  ]
-	};
+    return {
+      question_banks: [
+        {name: "kxz1", id: "1", icon: "mdi-library", brief: "hahahahha", details: {questions: "100", difficulty: "5", content: "this is all bullshit"}},
+        {name: "kxz2", id: "2", icon: "mdi-folder", brief: "hohohoho", details: {questions: "1", difficulty: "4", content: "get out"}},
+        {name: "kxz3", id: "3", icon: "mdi-folder", brief: "hehehehe"},
+        {name: "kxz4", id: "4", icon: "mdi-folder", brief: "hihihihi"},
+        {name: "kxz5", id: "5", icon: "mdi-folder", brief: "huhuhuhu"}
+      ]
+    };
   },
   components: {
-	"question-banks-list": QuestionBanksList
-  },
-}
+    "question-banks-list": QuestionBanksList
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
