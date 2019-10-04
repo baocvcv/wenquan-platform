7bc57f4acf35d565866cd84efadbb857154eb310
孔祥哲 <2018011997@secoder.net>
2019-10-04 19:41:52 +0800

    Add question banks list component #28

---
+++ frontend/src/components/QuestionBanksList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+	<v-list two-line subheader>
+	  <v-subheader inset>Question Banks</v-subheader>
+
+	  <v-list-item
+	    v-for="qst_bank in question_banks"
+		:key="qst_bank.name"
+		@click="redirect(qst_bank.id)"
+	  >
+	    <v-list-item-avatar>
+		  <v-icon v-text="qst_bank.icon"
+		  ></v-icon>
+		</v-list-item-avatar>
+
+		<v-list-item-content>
+		  <v-list-item-title v-text="qst_bank.name"></v-list-item-title>
+		  <v-list-item-subtitle v-text="qst_bank.brief"></v-list-item-subtitle>
+		</v-list-item-content>
+
+		<v-list-item-action @click="redirct('/about')">
+		  <v-btn icon>
+			<v-icon color="grey lighten-1">mdi-information</v-icon>
+		  </v-btn>
+		</v-list-item-action>
+	  </v-list-item>
+	</v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {
+    question_banks: {} 
+  },
+  data: function(){
+	return {
+	  
+	};
+  },
+  methods: {
+	redirct(path) {
+	  this.$router.replace(path);
+	}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
+
---
+++ frontend/src/views/QuestionBanks.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+	<h1>shit</h1>
+    <question-banks-list :question_banks="question_banks"></question-banks-list>
+  </div>
+</template>
+
+<script>
+import QuestionBanksList from "../components/QuestionBanksList.vue";
+
+export default {
+  name: "",
+  data: function() {
+	return {
+	  question_banks: [
+	    {name: "kxz1", id: "1", icon: "mdi-folder", brief: "hahahahha"},
+		{name: "kxz2", id: "2", icon: "mdi-folder", brief: "hohohoho"},
+		{name: "kxz3", id: "3", icon: "mdi-folder", brief: "hehehehe"},
+		{name: "kxz4", id: "4", icon: "mdi-folder", brief: "hihihihi"}
+	  ]
+	};
+  },
+  components: {
+	"question-banks-list": QuestionBanksList
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
