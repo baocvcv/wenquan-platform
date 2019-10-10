1d76faa34119fa304d9306bdd7b9ca8fa34c00e4
Chen_Yuxiang <chenyuxi18@mails.tsinghua.edu.cn>
2019-10-10 17:36:32 +0800

    Rebuild the database --Cyx

--- WenQuan_Platform/settings.py
+++ WenQuan_Platform/settings.py
@@ -73,12 +73,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'wen_quan',
-        'NAME': 'postgres',
+        'NAME': 'wen_quan',
         'USER': 'postgres',
         'PASSWORD': '123',
-        # 'HOST': 'postgres.Never404.secoder.local',
-        'HOST': 'localhost',
+        'HOST': 'postgres.Never404.secoder.local',
+        # 'HOST': 'localhost',
         'PORT': '5432',
     }
 }
