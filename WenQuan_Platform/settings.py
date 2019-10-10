5f597bed6c91f0e917fc0e1025ba68af2ad0d2c2
Chen_Yuxiang <chenyuxi18@mails.tsinghua.edu.cn>
2019-10-10 16:58:06 +0800

    Cyx

--- WenQuan_Platform/settings.py
+++ WenQuan_Platform/settings.py
@@ -30,19 +30,7 @@ ALLOWED_HOSTS = [
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'backend',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'polymorphic',
-    'corsheaders'
-]
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles', 'backend', 'rest_framework', 'rest_framework.authtoken', 'polymorphic', 'corsheaders']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -89,8 +77,8 @@ DATABASES = {
         # 'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '123',
-        'HOST': 'postgres.Never404.secoder.local',
-        # 'HOST': 'localhost',
+        # 'HOST': 'postgres.Never404.secoder.local',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
@@ -153,6 +141,4 @@ REST_FRAMEWORK = {
 
 # authentication settings
 AUTH_USER_MODEL = 'backend.User'
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-)
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend', )
