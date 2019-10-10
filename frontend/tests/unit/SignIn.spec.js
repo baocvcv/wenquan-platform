545d756f1abc420ee3c57e16c59089b2e8b05311
鄢涤非 <2018010258@secoder.net>
2019-10-10 19:29:32 +0800

    Modify sign in unit test for rendering. #62

--- frontend/tests/unit/SignIn.spec.js
+++ frontend/tests/unit/SignIn.spec.js
@@ -1,31 +1,62 @@
-import { mount } from "@vue/test-utils";
+import { mount,createLocalVue } from "@vue/test-utils";
 import SignInBox from "@/components/SignInBox.vue";
 import SignIn from "@/views/SignIn.vue";
+import Vuetify from "vuetify";
+import Vuex from "vuex";
+import Vue from "vue";
+
+const localVue = createLocalVue();
+Vue.use(Vuetify);
+Vue.use(Vuex);
 
 describe("SignInBox.vue", () => {
+    let vuetify;
+    beforeEach(() => {
+        vuetify = new Vuetify();
+    });
     it("Correct Input", () => {
-        const wrapper=mount(SignInBox);
+        const wrapper=mount(SignInBox, {
+            localVue,
+            vuetify,
+            sync: false
+        });
         wrapper.setData({
             username:"test",
             password:"test"
         });
-        expect(wrapper.html()).toContain("<v-btn>");
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.contains(".v-btn--disabled")).toBe(false);
+        });
     });
     it("Wrong Input", () => {
-        const wrapper=mount(SignInBox);
+        const wrapper=mount(SignInBox, {
+            localVue,
+            vuetify,
+            sync: false
+        });
         wrapper.setData({
             username:"test",
             password:""
         });
-        expect(wrapper.html()).toContain('<v-btn disabled="true">');
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.contains(".v-btn--disabled")).toBe(true);
+        });
     });
 });
 
 describe("SignIn.vue", () => {
+    let vuetify;
+    beforeEach(() => {
+        vuetify = new Vuetify();
+    });
     it("Render Component Correctly", () => {
-        const wrapper=mount(SignIn);
-        expect(wrapper.findAll("div").at(1).classes()).toContain("sign-in-box");
-        expect(wrapper.contains("v-text-field")).toBe(true);
-        expect(wrapper.contains("v-btn")).toBe(true);
+        const wrapper=mount(SignIn, {
+            localVue,
+            vuetify,
+            sync: false
+        });
+        expect(wrapper.contains(".sign-in-box")).toBe(true);
+        expect(wrapper.contains(".v-text-field")).toBe(true);
+        expect(wrapper.contains(".v-btn")).toBe(true);
     });
 });
