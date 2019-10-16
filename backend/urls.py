5187c0ac3e533cc8d2a82eb8b064a8dad6f1d408
baocvcv <baocvcv@gmail.com>
2019-10-16 19:05:08 +0800

    Modified urls

--- backend/urls.py
+++ backend/urls.py
@@ -9,8 +9,8 @@ from backend import views
 
 urlpatterns = [
     # path('', include(router.urls)),
-    path(r'jwt-auth/', views.auth_views.CustomAuthToken.as_view(), name='account-auth'),
+    path(r'api/jwt-auth/', views.auth_views.CustomAuthToken.as_view(), name='account-auth'),
     path(r'jwt-auth2/', auth_views.obtain_auth_token),
-    path(r'accounts/users/', views.UserList.as_view(), name='user-list'),
-    path(r'accounts/users/<int:pk>/', views.UserDetail.as_view, name='user-detail'),
+    path(r'api/accounts/users/', views.UserList.as_view(), name='user-list'),
+    path(r'api/accounts/users/<int:pk>/', views.UserDetail.as_view, name='user-detail'),
 ]
