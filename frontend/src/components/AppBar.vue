6883fa743772df92ff2ef0ac6851bf4ec19db01e
夏奇 <2018013411@secoder.net>
2019-10-21 00:39:12 +0800

    Refactor the AppBar component into a more developer-friendly one #87

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -31,56 +31,56 @@ export default {
     name: "app-bar",
     data: function(){
         return {
-            nav_links: [
-                {
-                    name: "Home",
-                    text: "Home",
-                    link: "/",
-                    icon: "mdi-home",
-                    signin_required: false
-                },
-                {
-                    name: "About",
-                    text: "About",
-                    link: "/about",
-                    icon: "mdi-information",
-                    signin_required: false
-                },
-                {
-                    name: "User Management",
-                    text: "User Management",
-                    link: "/admin/usermanagement",
-                    group_required: ["Admin","SuperAdmin"],
-                    icon: "mdi-account-supervisor",
-                },
-                {
-                    name: "Question Banks",
-                    text: "Question Banks",
-                    link: "/admin/questionbanks",
-                    group_required: ["Student","Admin","SuperAdmin"],
-                    icon: "mdi-bank"
-                },
-                {
-                    name: "Sign up",
-                    text: "Sign up",
-                    link: "/signup",
-                    icon: "mdi-login",
-                    signin_required: false
-                },
-                {
-                    name: "Sign in",
-                    text: "Sign in",
-                    link: "/signin",
-                    icon: "mdi-login",
-                    signin_required: false
-                },
-                {
-                    name: "User",
-                    text: "Account",
-                    link: "/account",
-                    icon: "mdi-account"
-                }
-            ]
+            nav_link_groups: {
+                not_signed_in: [
+                    {
+                        name: "Home",
+                        text: "Home",
+                        link: "/",
+                        icon: "mdi-home",
+                    },
+                    {
+                        name: "About",
+                        text: "About",
+                        link: "/about",
+                        icon: "mdi-information"
+                    },
+                    {
+                        name: "Sign in",
+                        text: "Sign in",
+                        link: "/signin",
+                        icon: "mdi-login"
+                    },
+                    {
+                        name: "Sign up",
+                        text: "Sign up",
+                        link: "/signup",
+                        icon: "mdi-login"
+                    }
+                ],
+                student: [
+                    {
+                        name: "Account",
+                        text: "Account",
+                        link: "/account",
+                        icon: "mdi-account"
+                    }
+                ],
+                admin: [
+                    {
+                        name: "User Management",
+                        text: "User Management",
+                        link: "/admin/usermanagement",
+                        icon: "mdi-account-supervisor"
+                    },
+                    {
+                        name: "Question Banks",
+                        text: "Question Banks",
+                        link: "/admin/questionbanks",
+                        icon: "mdi-bank"
+                    }
+                ]
+            }
         };
     },
     mounted(){
@@ -95,24 +95,12 @@ export default {
             return _user;
         },
         rendered_nav_links: function() {
-            console.log(this.user);
-            return this.nav_links.filter(nav_link => {
-                let flag = true;
-                let signin_required = true;
-                if ("signin_required" in nav_link)
-                    signin_required = nav_link["signin_required"];
-                if (!this.user && signin_required)
-                    flag = false;
-                let auth_required = nav_link.auth_required;
-                if (this.user && auth_required && !this.user.user_permissions[auth_required])
-                    flag = false;
-                let group_required = nav_link.group_required;
-                if (this.user && group_required && group_required.indexOf(this.user.user_group)==-1)
-                    flag = false;
-                if (this.user && (nav_link["name"] === "Sign up" || nav_link["name"] === "Sign in"))
-                    flag = false;
-                return flag;
-            });
+            if (!this.user)
+                return this.nav_link_groups.not_signed_in;
+            else if (this.user.user_group === "Student")
+                return this.nav_link_groups.student;
+            else if (this.user.user_group === "Admin" || this.user.user_group === "SuperAdmin")
+                return this.nav_link_groups.admin;
         }
     },
     methods: {
---
+++ frontend/src/views/Admin.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>This is the home for Admin.</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
