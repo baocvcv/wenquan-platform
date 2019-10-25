cb0c3828ca8399b2edfb19e7856d3c1e20810e3f
孔祥哲 <2018011997@secoder.net>
2019-10-25 11:11:10 +0800

    Fix servic-worker.js GET problems #111

--- WenQuan_Platform/frontend.py
+++ WenQuan_Platform/frontend.py
@@ -4,17 +4,24 @@
 This is for frontend router navigation
 All urls that doesn't conform to backend url patterns will be dealt with here
 '''
-from django.shortcuts import render, redirect
-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from .settings import STATICFILES_DIRS
 def index(request, url):
     '''
     if 'static' or 'img' or 'manifest' is found, redirect them to static files,
     else, let frontend(index.html) deal with them
     '''
     url = request.path
-    keywords = ("static", "img", "manifest")
+    keywords = ("static", "img", "manifest.json")
     for keyword in keywords:
         pos = url.find(keyword)
-        if pos != -1 :
-            return redirect("/frontend/"+url[pos:])
+        if pos != -1:
+            return HttpResponseRedirect("/frontend/"+url[pos:])
+    not_allow_redirect_keywords = ("service-worker", "precache")
+    for keyword in not_allow_redirect_keywords:
+        pos = url.find(keyword)
+        if pos != -1:
+            content = open(STATICFILES_DIRS[0] + "/" + url[pos:]).read()
+            return HttpResponse(content, content_type="text/javascript")
     return render(request, "index.html")
