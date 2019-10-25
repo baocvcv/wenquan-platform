0b25a19d3a3d96d86379868756f7e14f92640c75
baocvcv <baocvcv@gmail.com>
2019-10-26 00:25:19 +0800

    Add change password view

--- WenQuan_Platform/settings.py
+++ WenQuan_Platform/settings.py
@@ -76,8 +76,8 @@ DATABASES = {
         'NAME': 'wen_quan',
         'USER': 'postgres',
         'PASSWORD': '123',
-        # 'HOST': 'postgres.Never404.secoder.local',
-        'HOST': 'localhost',
+        'HOST': 'postgres.Never404.secoder.local',
+        # 'HOST': 'localhost',
         'PORT': '5432',
     }
 }
@@ -136,7 +136,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    #  'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
 }
 
 # authentication settings
--- backend/views/change_password_view.py
+++ backend/views/change_password_view.py
@@ -1,4 +1,4 @@
-""" Views for verification """
+""" Views for password change"""
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,30 +9,27 @@ from backend.models import EmailVerificationRecord
 from backend.serializers import UserSerializer
 from backend.scripts.email_verification import create_email_verification_record
 
-#TODO: split password change from this
-class EmailVerificationView(APIView):
-    """ Perform email verification """
+# TODO: change this
+class ChangePasswordView(APIView):
+    """ Handle password change """
     def get(self, request):
         """ send verification email """
         user = request.user
-        if user.is_active == False:
-            create_email_verification_record(user)
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        create_email_verification_record(user, send_type='forget')
+        return Response(status=status.HTTP_200_OK)
 
     def post(self, request):
         """ make modifications """
         token = request.data['token']
         record = EmailVerificationRecord.objects.get(token=token)
-        if record is None or record.is_valid is False:
+        if record is None or record.is_valid == False:
             return Response(
                 {"token": "Token does not exist"},
                 status=status.HTTP_400_BAD_REQUEST)
         user = record.user
-        if record.send_type == "register":
+        if record.send_type == "forget":
             if record.is_time_valid(timezone.now()):
-                user.is_active = True
+                user.set_password(request.data['password'])
                 user.save()
                 record.is_valid = False
                 record.save()
