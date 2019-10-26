3710c123cce24ddf2d390a3cab0fa657f37f1092
Chen_Yuxiang <chenyuxi18@mails.tsinghua.edu.cn>
2019-10-26 13:45:52 +0800

    Modify code to pass pylint  --Cyx

--- backend/urls.py
+++ backend/urls.py
@@ -13,10 +13,30 @@ urlpatterns = [
     path(r'jwt-auth2/', auth_views.obtain_auth_token),
     path(r'api/accounts/users/', views.UserList.as_view(), name='user-list'),
     path(r'api/accounts/users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path(r'api/questions/', views.QuestionList.as_view(), name='questions_list'),
     path(r'api/verification/', views.EmailVerificationView.as_view(), name='verification'),
-    path(r'api/questions/<int:pk>/', views.QuestionDetail.as_view(), name='questions_list'),
-    path(r'api/nodes_list/<int:pk>/', views.KnowledgeNodeList.as_view(), name='nodes_list'),
-    path(r'api/question_banks/', views.QuestionBankList.as_view(), name='banks_list'),
-    path(r'api/question_banks/<int:pk>/', views.QuestionBankDetail.as_view(), name='banks_list'),
+    path(
+        r'api/questions/',
+        views.QuestionList.as_view(),
+        name='questions_list',
+    ),
+    path(
+        r'api/questions/<int:q_id>/',
+        views.QuestionDetail.as_view(),
+        name='questions_list',
+    ),
+    path(
+        r'api/nodes_list/<int:root_id>/',
+        views.KnowledgeNodeList.as_view(),
+        name='nodes_list',
+    ),
+    path(
+        r'api/question_banks/',
+        views.QuestionBankList.as_view(),
+        name='banks_list',
+    ),
+    path(
+        r'api/question_banks/<int:bank_id>/',
+        views.QuestionBankDetail.as_view(),
+        name='banks_list',
+    ),
 ]
--- backend/views/knowledge_node_views.py
+++ backend/views/knowledge_node_views.py
@@ -2,15 +2,19 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
+from django.http import Http404
 
-from backend.serializers.knowledge_node_serializer import KnowlegdeNodeSerializer
 from backend.models.knowledge_node import KnowledgeNode
 
 
 class KnowledgeNodeName(APIView):
-    """Only get nodes' names and ids"""
-    def get(self, request, pk):
-        node = KnowledgeNode.objects.get('id=pk')
+    """Get Brief infomation of a KnowledgeNode"""
+    def get(self, request, root_id):
+        """Only get id and name of node whose id=root_id"""
+        try:
+            node = KnowledgeNode.objects.get(id=root_id)
+        except KnowledgeNode.DoesNotExist:
+            raise Http404
         response = {}
         response['id'] = node.id
         response['name'] = node.name
@@ -18,26 +22,40 @@ class KnowledgeNodeName(APIView):
 
 
 class KnowledgeNodeList(APIView):
-    """Get all Node info or create a question"""
+    """View for all KnowledgeNode"""
     tree = []
 
+    @classmethod
+    def get_object(cls, root_id):
+        """Get the KnowledgeNode with id=root_id"""
+        try:
+            return KnowledgeNode.objects.get(id=root_id)
+        except KnowledgeNode.DoesNotExist:
+            raise Http404
+
     def go_through_tree(self, root_id):
-        root = KnowledgeNode.objects.get(id=root_id)
+        """KnowledgeNode tree level order traversal"""
+        root = self.get_object(root_id)
         json = {}
         child_json = []
-        json['id'] = root_id
+        json['id'] = root.id
+        json['name'] = root.name
         child = root.subnodes.all()
-        while len(child) != 0:
+        while child:
             for i in child:
-                child_json += go_through_tree(child.id)
+                child_json += self.go_through_tree(i.id)
         json['subnodes'] = child_json
         return json
 
-    def get(self, request, pk):
-        response = self.go_through_tree(pk)
-        response['bank_id'] = KnowledgeNode.objects.get(id=pk).question_bank.id
+    def get(self, request, root_id):
+        """Get a tree whose root's id = root_id"""
+        response = self.go_through_tree(root_id)
+        response['bank_id'] = self.get_object(root_id).question_bank.id
         return Response(response)
 
-    def post(self, request, pk):
+    def post(self, request):
         """Create a KnowledgeNode"""
         post_data = JSONParser().parse(request)[0]
+        self.go_through_tree(1)
+        response = post_data
+        return Response(response)
