4b8879fdee3722a8fc92cd39e17a4af84368aa67
HSIA CHI <2018013411@secoder.net>
2019-11-01 14:44:30 +0800

    Add some more cases for question bank #118

--- frontend/tests/unit/QuestionBank.spec.js
+++ frontend/tests/unit/QuestionBank.spec.js
@@ -1,5 +1,6 @@
 import {mount, createLocalVue} from "@vue/test-utils";
 import QuestionBank from "@/views/QuestionBank.vue";
+import Home from "@/views/Home.vue";
 import Vue from "vue";
 import Vuex from "vuex";
 import Vuetify from "vuetify";
@@ -17,7 +18,14 @@ describe("QuestionBank.vue", () => {
 
     beforeEach(() => {
         vuetify = new Vuetify();
-        router = new Router({RouterRule});
+        //router = new Router({RouterRule});
+        router = new Router({
+            routes: [
+                { path: "/admin/questionbanks/:id", name: "question-bank", component: QuestionBank},
+                { path: "/", component: Home}
+            ]
+        }
+        )
     })
 
     it("Failed to fetch data", async (done) => {
@@ -91,7 +99,7 @@ describe("QuestionBank.vue", () => {
                 })
                 console.log(wrapper.vm.edited_question_bank.name);
                 wrapper.vm.save();
-                //save_button.trigger("click");
+                // save_button.trigger("click");
                 setTimeout(() => {
                     expect(wrapper.vm.edit_mode).toBe(false);
                     expect(wrapper.vm.question_bank.name).toBe("edited");
@@ -99,5 +107,84 @@ describe("QuestionBank.vue", () => {
                 }, 1000)
             }, 500)
         }, 500)
+    })
+
+    it("Pops up an alert before discarding changes", async (done) => {
+        sessionStorage.setItem("user", {
+            user_group: "Admin"
+        })
+        router.push({name: "question-bank", params: {id: 200}})
+        let wrapper = mount(QuestionBank, {
+            localVue,
+            vuetify,
+            router,
+            sync: false,
+        })
+        setTimeout(async () => {
+            window.confirm = jest.fn(() => {
+                return false;
+            })
+            let edit_button = wrapper.find(".mdi-pencil");
+            edit_button.trigger("click");
+            await wrapper.vm.$nextTick();
+            wrapper.setData({
+                edited_question_bank_image: ["image"],
+                edited_question_bank: {
+                    name: "edited"
+                }
+            })
+            await wrapper.vm.$nextTick();
+            expect(wrapper.vm.edit_mode).toBe(true);
+            expect(wrapper.vm.edited).toBe(true);
+            edit_button.trigger("click");
+            await wrapper.vm.$nextTick();
+            expect(window.confirm).toHaveBeenCalled();
+            expect(wrapper.vm.edit_mode).toBe(true);
+            window.confirm = jest.fn(() => {
+                return true;
+            })
+            edit_button.trigger("click");
+            await wrapper.vm.$nextTick()
+            expect(window.confirm).toHaveBeenCalled();
+            expect(wrapper.vm.edit_mode).toBe(false);
+            done();
+        },1000)
+    })
+
+    it("Pops up an alert when leaving current route with unsaved changes", async (done) => {
+        sessionStorage.setItem("user", {
+            user_group: "Admin"
+        })
+        router.push({name: "question-bank", params: {id: 200}})
+        let wrapper = mount(QuestionBank, {
+            localVue,
+            vuetify,
+            router,
+            sync: false,
+        })
+        setTimeout(async () => {
+            let edit_button = wrapper.find(".mdi-pencil");
+            edit_button.trigger("click");
+            await wrapper.vm.$nextTick();
+            wrapper.setData({
+                edited_question_bank_image: ["edited_image"]
+            })
+            await wrapper.vm.$nextTick();
+            expect(wrapper.vm.edited).toBe(true);
+            window.confirm = jest.fn(() => {
+                return false;
+            })
+            wrapper.vm.$router.push("/");
+            await wrapper.vm.$nextTick();
+            console.log(wrapper.vm.$route.path);
+            //expect(window.confirm).toHaveBeenCalled();
+            //expect(wrapper.vm.$route.path === "/").toBe(false);
+            window.confirm = jest.fn(() => {
+                return true;
+            })
+            await wrapper.vm.$nextTick();
+            //expect(wrapper.vm.$route.path).toBe("/");
+            done();
+        }, 1000)
     })
 })
