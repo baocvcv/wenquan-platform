ab4ac84e3783be122454c9ab0619b22b150dc47c
HSIA CHI <2018013411@secoder.net>
2019-11-03 01:25:04 +0800

    Modify the UI of sign in box #156

--- frontend/src/components/SignInBox.vue
+++ frontend/src/components/SignInBox.vue
@@ -1,23 +1,39 @@
 <template>
   <div class="sign-in-box">
-    <v-form ref="input">
-      <v-text-field
-        v-model="username"
-        label="用户名"
-        v-bind:rules="[v => !!v || 'Username cannot be empty']"
-      ></v-text-field>
-      <v-text-field
-        v-model="password"
-        label="密码"
-        v-bind:type="show_password ? 'text' : 'password'"
-        v-bind:rules="[v => !!v || 'Password cannot be empty']"
-        v-bind:append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'"
-        v-on:click:append="show_password = !show_password"
-      ></v-text-field>
-      <v-btn v-on:click="click" v-bind:disabled="!username || !password"
-        >Sign In</v-btn
-      >
-    </v-form>
+        <v-card>
+          <v-card-text>
+            <p class="text-center display-1 font-weight-bold text--primary text-uppercase">sign in</p>
+            <v-container>
+              <v-form ref="input">
+                <v-text-field
+                  v-model="username"
+                  label="Username"
+                  v-bind:rules="[v => !!v || 'Username cannot be empty']"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  v-bind:type="show_password ? 'text' : 'password'"
+                  v-bind:rules="[v => !!v || 'Password cannot be empty']"
+                  v-bind:append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'"
+                  v-on:click:append="show_password = !show_password"
+                ></v-text-field>
+                <p class="text-center">
+                  <router-link to="/"><small>Forget Password?</small></router-link> | 
+                  <router-link to="/signup"><small>Do not have an account yet? click here to sign up.</small></router-link>
+                </p>
+                <v-row>
+                  <v-spacer></v-spacer>
+                  <v-btn outlined v-on:click="click" v-bind:disabled="!username || !password"
+                    >Sign In</v-btn
+                  >
+                  <v-spacer></v-spacer>
+                </v-row>
+              </v-form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+    
     <v-dialog v-model="show_dialog" max-width="300" data-app>
       <v-card>
         <v-toolbar color="indigo" dark>
