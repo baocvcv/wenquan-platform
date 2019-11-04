0502ab925505b5c053b39fd35a7c0a21b529d9d6
baocvcv <baocvcv@gmail.com>
2019-11-05 00:29:27 +0800

    Fix pytest errors

--- backend/tests/test_change_password.py
+++ backend/tests/test_change_password.py
@@ -21,16 +21,14 @@ class TestChangePasswordView(APITestCase):
         self.client.post(url1, USER_DATA, format='json')
         # activate
         activate_all_users()
-        # login
-        self.client.login(username=USER_DATA['username'], password=USER_DATA['password'])
         # get change password token
         url2 = reverse('password')
-        response2 = self.client.get(url2)
+        response2 = self.client.post(url2, USER_DATA)
         self.assertEqual(response2.status_code, status.HTTP_200_OK)
         # change passwd
         verification_token = EmailVerificationRecord.objects.get(send_type='forget').token
         data = {'token': verification_token, 'password': 'abcd1111'}
-        response3 = self.client.post(url2, data, format='json')
+        response3 = self.client.put(url2, data, format='json')
         self.assertEqual(response3.status_code, status.HTTP_200_OK)
         # auth again
         USER_DATA['password'] = 'abcd1111'
