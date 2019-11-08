d840b84e77fc2f9c0c7b5629478db053bc7a75fe
HSIA CHI <2018013411@secoder.net>
2019-11-08 15:48:25 +0800

    Fetch questions according to the selected nodes #159

--- frontend/src/components/QuestionList.vue
+++ frontend/src/components/QuestionList.vue
@@ -133,7 +133,7 @@
       <v-card-text>
         <vue-progress-bar style="position: relative;"></vue-progress-bar>
         <v-row>
-          <v-col v-show="drawer" cols="12" md="4" sm="6">
+          <v-col v-show="drawer" cols="12" md="4" sm="6" id="drawer-col">
             <tree-view
               :bankID="id"
               v-model="tree_selection"
@@ -152,7 +152,7 @@
             </p>
             <v-row dense>
               <v-col
-                v-for="question in question_list"
+                v-for="question in shown_questions"
                 :key="question.id"
                 cols="12"
               >
@@ -222,7 +222,7 @@ export default {
       level_min_filter: 0,
       level_max_filter: 5,
       drawer: null,
-      question_list: [],
+      question_list: {},
       tree_selection: [],
       sort_menu: ["Popularity", "Level"],
       keyword: "",
@@ -235,53 +235,98 @@ export default {
       ],
       selected_questions: [],
       is_selecting: false,
-      process: ""
+      process: "",
+      shown_questions: [],
+      question_indices: []
     };
+  },
+  watch: {
+    tree_selection() {
+      this.process = "Fetching data from server...";
+      let selected_node_id = [];
+      let i;
+      for (i in this.tree_selection)
+      {
+        selected_node_id.push(this.tree_selection[i].id);
+      }
+      if (selected_node_id.length === 0)
+      {
+        this.shown_questions = [];
+        let index;
+        for (index in this.question_list)
+        {
+          this.shown_questions.push(this.question_list[index]);
+        }
+        this.process = "Total Count: " + this.shown_questions.length;
+        return;
+      }
+      axios
+        .post("/api/nodes_question/", {"nodes_id": selected_node_id})
+        .then(response => {
+          this.question_indices = response.data;
+        })
+    },
+    question_indices() {
+      this.shown_questions = [];
+      let count = 0;
+      let all_count = this.question_indices.length;
+      let lock = false;
+      let i;
+      this.$Progress.start();
+      console.log(this.process);
+      let process_on = () => {
+        while (lock);
+        lock = true;
+        count++;
+        this.$Progress.increase(1 / (all_count + 0.0001) * 100);
+        this.process = "Loading questions: " + count + " / " + all_count;
+        if (count == all_count) {
+          this.process = "Total Count: " + all_count;
+          this.$Progress.finish();
+        }
+        lock = false;
+      }
+      console.log(this.process);
+      if (this.question_indices.length === 0)
+      {
+        this.$Progress.finish();
+        this.process = "Total Count: " + 0;
+      }
+      for (i in this.question_indices)
+      {
+        let index = this.question_indices[i];
+        if (this.question_list.hasOwnProperty(index))
+        {
+          this.shown_questions.push(this.question_list[index]);
+          process_on();
+        }
+        else
+        {
+          axios
+            .get("/api/questions/" + index + "/")
+            .then(response => {
+              this.shown_questions.push(response.data);
+              this.question_list[index] = response.data;
+              process_on();
+            })
+        }
+      }
+    }
   },
   mounted() {
     if (this.select) this.is_selecting = true;
     this.process = "Fetching data from server...";
-    let load_questions = () => {
-      let question_id_index;
-      let all_count = questions.length;
-      let count = 0;
-      let lock = false;
-      this.$Progress.set(0);
-      for (question_id_index in questions) {
-        axios
-          .get("/api/questions/" + questions[question_id_index] + "/")
-          .then(sub_response => {
-            this.question_list.push(sub_response.data);
-            while (lock);
-            lock = true;
-            count++;
-            lock = false;
-            this.process = "Loading questions: " + count + " / " + all_count;
-            this.$Progress.increase((1 / all_count) * 100);
-            if (count == all_count) {
-              this.process = "Total Count: " + all_count;
-              this.$Progress.finish();
-            }
-          })
-          .catch(error => {
-            this.process = "Failed to fetch data: " + error;
-          });
-      }
-    };
-    let questions;
     if (this.questions.length == 0) {
       axios
         .get("/api/question_banks/" + this.id + "/")
         .then(response => {
-          questions = response.data.questions;
-          load_questions(response);
+          this.question_indices = JSON.parse(JSON.stringify(this.questions));
         })
         .catch(error => {
           console.log(error);
         });
     } else {
-      questions = this.questions;
-      load_questions();
+      this.question_indices = JSON.parse(JSON.stringify(this.questions));
     }
   },
   methods: {
@@ -294,7 +339,17 @@ export default {
       axios
         .get("/api/questions/" + question_id + "/")
         .then(response => {
-          this.question_list.push(response.data);
+          this.question_list[question_id] = response.data;
+          let node_index;
+          for (node_index in response.data.parent_nodes)
+          {
+            let selected_index;
+            for (selected_index in this.tree_selection)
+            {
+              if (this.tree_selection[selected_index] === response.data.parent_nodes[node_index])
+                this.shown_questions.push(response.data);
+            }
+          }
           this.create_question_dialog = false;
         })
         .catch(error => {
@@ -313,3 +368,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+#drawer-col {
+  border-right: 1px solid rgb(226, 226, 226);
+}
+</style>
