bfb4bca0320d9c6c92592f1ebd999c277adcd1b1
鄢涤非 <2018010258@secoder.net>
2019-11-12 16:06:08 +0800

    Add base view of question and a test view. #179

---
+++ frontend/src/components/QuestionSolve.vue
@@ -0,0 +1,20 @@
+<template>
+    <question-show :id="3">
+        <template v-slot:button>
+            <v-list-item>
+                A Slot Example
+            </v-list-item>
+        </template>
+    </question-show>
+</template>
+
+<script>
+import QuestionShow from "./QuestionShow.vue"
+
+export default {
+    name: "question-solve",
+    components: {
+        "question-show": QuestionShow
+    }
+}
+</script>
