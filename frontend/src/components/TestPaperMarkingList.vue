43651081f6fd8203a669d579ad0b2d7fee06f3f1
孔祥哲 <2018011997@secoder.net>
2019-11-13 09:47:31 +0800

    Initialize test paper marking list view #181

---
+++ frontend/src/components/TestPaperMarkingList.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card>
+    <v-toolbar flat>
+	  <v-toolbar-title>{{ title }}</v-toolbar-title>
+	  <v-spacer></v-spacer>
+	  <v-text-field
+	    flat
+		hide-details
+		label="Search"
+		prepend-inner-icon="mdi-magnify"
+		clearable
+	  ></v-text-field>
+	</v-toolbar>
+	<v-card-text class="pt-0">
+	  <p
+	    class="caption grey--text text-right mt-0 mb-0 pr-1"
+		transition="fade-transition"
+	  >
+	    {{ process }}
+	  </p>
+	  <v-list>
+	  </v-list>
+  </v-card>
+</template>
+
+<script>
+import TestPaperMarkingListItem from "@/components/TestPaperMarkingListItem.vue";
+export default {
+  name: "",
+  props: {
+	title: {
+	  type: String,
+	  default: "Marking"
+    }
+  },
+  components: {
+	"test-paper-marking-list-item": TestPaperMarkingListItem
+  },
+  data: function() {
+	return {
+	  process: "loading"
+	};
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
+
---
+++ frontend/src/components/TestPaperMarkingListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list-group>
+    <template v-slot:activator>
+	  <v-list-item-title>Test Paper name</v-list-item-title>
+	</template>
+	<v-list-item>
+	</v-list-item>
+  </v-list-group>
+</template>
+
+<script>
+export default {
+  name: "",
+  props: {
+	id: -1
+  },
+  components: {
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
+
