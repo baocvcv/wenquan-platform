353bddcc85384b68a86570234adf737772b2d7df
孔祥哲 <2018011997@secoder.net>
2019-11-13 10:23:19 +0800

    Initialize self-practice view #182

---
+++ frontend/src/components/Practice.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-stepper v-model="step">
+      <v-stepper-header>
+        <v-stepper-step :complete="step > 1" step="1"
+          >Choose a Question Bank</v-stepper-step
+        >
+        <v-divider></v-divider>
+        <v-stepper-step :complete="step > 2" step="2"
+          >Choose Number of Questions to Practice</v-stepper-step
+        >
+        <v-divider></v-divider>
+        <v-stepper-step step="3">Practice</v-stepper-step>
+      </v-stepper-header>
+    </v-stepper>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "practice",
+  props: {},
+  components: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ frontend/src/views/student/Learn.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-tabs>
+        <v-tab>
+          <v-icon left>mdi-finance</v-icon>
+          Practice
+        </v-tab>
+        <v-tab>
+          <v-icon left>mdi-clock-outline</v-icon>
+          Test
+        </v-tab>
+        <v-tab-item>
+          <question-banks-list
+            :flat="true"
+            :readonly="true"
+            select
+          ></question-banks-list>
+        </v-tab-item>
+        <v-tab-item>
+          <test-papers-list
+            :title="
+              $vuetify.breakpoint.smAndUp
+                ? 'Choose a Test Paper to Simulate'
+                : 'Test'
+            "
+          />
+        </v-tab-item>
+      </v-tabs>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import QuestionBanksList from "@/components/QuestionBanksList.vue";
+import TestPapersList from "@/components/TestPapersList.vue";
+export default {
+  name: "learn",
+  components: {
+    "question-banks-list": QuestionBanksList,
+    "test-papers-list": TestPapersList
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
