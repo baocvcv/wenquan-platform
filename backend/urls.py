4cd67755ff5718cf626079065d1e8da6ade55e23
baocvcv <baocvcv@gmail.com>
2019-11-13 08:45:09 +0800

    Fix pylint errors

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,6 @@
 # from django.urls import include
 from django.urls import path
 # from rest_framework import routers
-from rest_framework.authtoken import views as auth_views
 from backend import views
 
 # router = routers.DefaultRouter()
