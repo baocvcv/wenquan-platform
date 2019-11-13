e5fa78af76a89b507f1391d2f2138beecd879312
孔祥哲 <2018011997@secoder.net>
2019-11-13 16:21:21 +0800

    Add axios function of list item #181

--- frontend/src/components/TestPaperMarkingListItem.vue
+++ frontend/src/components/TestPaperMarkingListItem.vue
@@ -1,19 +1,43 @@
 <template>
-  <v-list-group>
+  <v-list-group v-if="!!paper_name">
     <template v-slot:activator>
-      <v-list-item-title>Test Paper name</v-list-item-title>
+      <v-list-item-title>{{ paper_name }}</v-list-item-title>
     </template>
-    <v-list-item> </v-list-item>
+    <v-list-item v-for="(record, key) in paper_records" :key="key">
+      <v-list-item-content>
+        <v-row align="center">
+          <v-col>{{ record.id }}</v-col>
+        </v-row>
+      </v-list-item-content>
+      <v-list-item-action> </v-list-item-action>
+    </v-list-item>
   </v-list-group>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: "",
+  name: "test-paper-marking-list-item",
   props: {
     id: -1
   },
-  components: {}
+  data: function() {
+    return {
+      paper_name: "",
+      paper_records: []
+    };
+  },
+  created() {
+    axios.get("/api/paper_records?paper=" + this.id).then(response => {
+      let all_records = response.data;
+      for (var i = 0; i < all_records.length; i++) {
+        if (all_records[i].is_active)
+          this.paper_records.unshift(all_records[i]);
+        else this.paper_records.push(all_records[i]);
+      }
+      if (all_records.length != 0) this.paper_name = all_records[0].paper_name;
+    });
+  }
 };
 </script>
 
