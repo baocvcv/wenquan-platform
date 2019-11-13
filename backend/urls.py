0d7234f0e70ec9ddede2c2bee6e34ae1f289e621
baocvcv <baocvcv@gmail.com>
2019-11-13 08:29:16 +0800

    Update urls

--- backend/urls.py
+++ backend/urls.py
@@ -18,65 +18,19 @@ urlpatterns = [
     path(r'api/verification/', views.EmailVerificationView.as_view(), name='verification'),
     path(r'api/password/', views.ChangePasswordView.as_view(), name='password'),
     # questions
-    path(
-        r'api/questions/',
-        views.QuestionList.as_view(),
-        name='questions_list',
-    ),
-    path(
-        r'api/questions/<int:q_id>/',
-        views.QuestionDetail.as_view(),
-        name='questions_detail',
-    ),
-    path(
-        r'api/nodes_list/<int:root_id>/',
-        views.KnowledgeNodeList.as_view(),
-        name='nodes_list',
-    ),
-    path(
-        r'api/knowledge_nodes/<int:root_id>/',
-        views.KnowledgeNodeDetail.as_view(),
-        name='node_detail',
-    ),
-    path(
-        r'api/nodes_question/',
-        views.NodeQuestionView.as_view(),
-        name='nodes_question',
-    ),
-    path(
-        r'api/question_banks/',
-        views.QuestionBankList.as_view(),
-        name='banks_list',
-    ),
-    path(
-        r'api/question_banks/<int:bank_id>/',
-        views.QuestionBankDetail.as_view(),
-        name='banks_list',
-    ),
-    path(
-        r'api/papers/',
-        views.PaperList.as_view(),
-        name='papers_list',
-    ),
-    path(
-        r'api/papers/<int:paper_id>/',
-        views.PaperDetail.as_view(),
-        name='paper_detail',
-    ),
-    path(
-        r'api/paper_sections/<int:section_id>/',
-        views.SectionDetail.as_view(),
-        name='section_detail',
-    ),
+    path(r'api/questions/', views.QuestionList.as_view(), name='questions_list',),
+    path(r'api/questions/<int:q_id>/', views.QuestionDetail.as_view(), name='questions_detail',),
+    path(r'api/nodes_list/<int:root_id>/', views.KnowledgeNodeList.as_view(), name='nodes_list',),
+    path(r'api/knowledge_nodes/<int:root_id>/', views.KnowledgeNodeDetail.as_view(), name='node_detail',),
+    path(r'api/nodes_question/', views.NodeQuestionView.as_view(), name='nodes_question',),
+    path(r'api/question_banks/', views.QuestionBankList.as_view(), name='banks_list',),
+    path(r'api/question_banks/<int:bank_id>/', views.QuestionBankDetail.as_view(), name='banks_list',),
+    path(r'api/papers/', views.PaperList.as_view(), name='papers_list',),
+    path(r'api/papers/<int:paper_id>/', views.PaperDetail.as_view(), name='paper_detail',),
+    path(r'api/paper_sections/<int:section_id>/', views.SectionDetail.as_view(), name='section_detail',),
     # records
-    path(
-        r'api/question_records/',
-        views.QuestionRecordList.as_view(),
-        name='question_record_list',
-    ),
-    path(
-        r'api/question_records/<int:pk>',
-        views.QuestionRecordDetail.as_view(),
-        name='question_record_detail',
-    ),
+    path(r'api/question_records/', views.QuestionRecordList.as_view(), name='question_record_list',),
+    path(r'api/question_records/<int:pk>', views.QuestionRecordDetail.as_view(), name='question_record_detail',),
+    path(r'api/paper_records/', views.PaperRecordList.as_view(), name="paper_record_list"),
+    path(r'api/paper_records/<int:pk>', views.PaperRecordDetail.as_view(), name="paper_record_detail"),
 ]
