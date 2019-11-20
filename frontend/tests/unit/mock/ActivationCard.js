f52e3a5ad9f74a6ba3d00d74c6d69c75bebcae90
鄢涤非 <2018010258@secoder.net>
2019-11-20 18:18:57 +0800

    Add unit test for activation card. #205

---
+++ frontend/tests/unit/mock/ActivationCard.js
@@ -0,0 +1,22 @@
+import nock from "nock";
+
+nock(/.*/)
+  .get("/api/auth_code/123", body => true)
+  .reply(200, {
+    token: "ok",
+  });
+nock(/.*/)
+  .get("/api/accounts/users/123/", body => true)
+  .reply(200, {
+    question_banks: [1,2,3],
+  });
+nock(/.*/)
+  .get("/api/auth_code/234", body => true)
+  .reply(400, {
+    token: "failed"
+  });
+nock(/.*/)
+  .get("/api/auth_code/345", body => true)
+  .reply(500, {
+    token: "failed"
+  });
