5e2f73e835dfd45c5bd6b7ca254caaffe7880e43
HSIA CHI <2018013411@secoder.net>
2019-11-20 10:06:56 +0800

    Mock the wheel to pass the test #204

---
+++ frontend/tests/unit/mock/RichTextEditorMock.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="mock">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RichTextEditor",
+    methods: {
+        reset() {
+            
+        }
+    }
+}
+</script>
