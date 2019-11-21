a27530848c30c5875f3e8001b67b4a8f51c605fe
鄢涤非 <2018010258@secoder.net>
2019-11-21 18:54:30 +0800

    fix timer bug/

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -108,16 +108,17 @@ export default {
               }
             }
           );
-        }
 
-        this.record_id = record.data.id;
-        this.time_left = record.data.time_left;
-        var timer = setInterval(() => {
-          if (this.time_left == 0) {
-            if (this.$refs && this.$refs.solve) this.$refs.solve.force_submit();
-            clearInterval(timer);
-          } else this.time_left--;
-        }, 1000);
+          this.record_id = record.data.id;
+          this.time_left = record.data.time_left;
+          var timer = setInterval(() => {
+            if (this.time_left == 0) {
+              if (this.$refs && this.$refs.solve)
+                this.$refs.solve.force_submit();
+              clearInterval(timer);
+            } else this.time_left--;
+          }, 1000);
+        }
 
         this.loading = false;
       })
